d995bc8362b813fe7bc2b679d79bbbfe972c1adc
unknown <igatatsu1997@gmail.com>
2022-02-18 14:07:54 +0900

    test

---
+++ src/components/controlPanel.js
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react"
+
+export const  ControlPanel = () => {
+  return(
+    <Box w={'100%'} minH='20%' bgColor={'grey.300'}></Box>
+  )
+
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from "./theme/theme";
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>,
@@ -19,4 +16,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
