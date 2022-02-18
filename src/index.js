639d05f64de21d4a438ecf2f39167e41678aa964
unknown <igatatsu1997@gmail.com>
2022-02-18 09:51:41 +0900

    introduced Chakra-ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { StrictMode } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from "./theme/theme";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
   document.getElementById('root')
 );
 
